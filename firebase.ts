edaf7d601e5e8eeea28d6cec71a429227353a536
paguevan-lgtm <paguevan@gmail.com>
2026-02-02 23:05:50 -0300

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,35 +1,22 @@
-// Access the global firebase object loaded via scripts in index.html
-const firebase = (window as any).firebase;
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyConFpC-5IPOO8dxrYggy8JusWtC7BsgWs",
-    authDomain: "lotacao-97c37.firebaseapp.com",
-    projectId: "lotacao-97c37",
-    databaseURL: "https://lotacao-97c37-default-rtdb.firebaseio.com",
-    storageBucket: "lotacao-97c37.appspot.com",
-    messagingSenderId: "782139851070",
-    appId: "1:782139851070:web:dd2f3327c33a45f443ec6f"
+  apiKey: "AIzaSyBTF5M7HcFIvF_TbFqpG7zMjh29AXP1zzM",
+  authDomain: "lotacao-753a1.firebaseapp.com",
+  databaseURL: "https://lotacao-753a1-default-rtdb.firebaseio.com",
+  projectId: "lotacao-753a1",
+  storageBucket: "lotacao-753a1.firebasestorage.app",
+  messagingSenderId: "755549088369",
+  appId: "1:755549088369:web:6182fc39adbd73ea4789d0",
+  measurementId: "G-3KWB2PQMCN"
 };
 
-let db: any;
-let auth: any;
-
-if (firebase) {
-    if (!firebase.apps.length) {
-        firebase.initializeApp(firebaseConfig);
-    }
-    // Safety check: ensure database module is attached
-    if (firebase.database) {
-        db = firebase.database();
-    } else {
-        console.error("Firebase Database module not found.");
-    }
-
-    if (firebase.auth) {
-        auth = firebase.auth();
-    }
-} else {
-    console.error("Firebase SDK not loaded");
-}
-
-export { db, auth, firebase };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
